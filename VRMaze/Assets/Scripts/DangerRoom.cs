dd206389147bb4563631872a01e87047e5a97c91
Chaimae Menouar <chaimae.menouar@grenoble-inp.org>
2022-10-07 11:16:48 +0200

    First danger block try

---
+++ VRMaze/Assets/Scripts/DangerRoom.cs
@@ -0,0 +1,66 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using PathCreation;
+
+public class DangerRoom : MonoBehaviour
+{
+    Animation animations;
+    Material material;
+    Vector3 directionToTarget;
+
+    public float radius = 100000000;
+
+    public GameObject playerRef;
+    public SoundManager soundManager;
+    public Transform player;
+
+    public LayerMask targetMask;
+    public LayerMask obstructionMask;
+
+    public PathCreator pathCreator;
+    public float distanceToTarget;
+
+    // 0 : Not in the room 
+    // 1 : Has entered the room 
+    // 2 : Entered the room and doors have been closed
+    private int playerPosStatus = 0; 
+
+    private void Start()
+    {
+        soundManager = FindObjectOfType<SoundManager>();
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+        animations =GetComponent<Animation>();
+    }
+
+
+    private void Update()
+    {
+        Collider[] rangeChecks = Physics.OverlapSphere(transform.position, radius, targetMask);
+
+        //Get the position of the block center
+        Vector3 centerPosition = transform.Find("ground_cross").position;
+        float limitDist =  transform.Find("ground_cross").GetComponent<Renderer>().bounds.size[0];
+
+        //Check the distance of the player from the block center
+        Vector3 playerPos = player.position;
+        distanceToTarget = Vector3.Distance(playerPos, centerPosition);
+
+        //If the distance is inferior to the threshold, activate the doors
+        if (distanceToTarget < limitDist/2){
+
+            //Change playerpos status to has just entered the room
+            if (playerPosStatus == 0) {
+                playerPosStatus = 1;
+            }
+
+            if (playerPosStatus == 1) {
+                animations.Play("closeDoors");
+                playerPosStatus = 2; //Change status so that the animation only plays once
+            }
+        }
+    }
+    
+}
+
